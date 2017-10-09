f4db77620e1d9c4d1d04b64b7267ea000cb6879a
vancanhuit <vancanhuit@gmail.com>
2017-10-09 16:05:12 +0700

    Add some utility functions

---
+++ utils.py
@@ -0,0 +1,91 @@
+""" Utility functions """
+
+import math
+
+
+def readWeatherDataSet(sourceFile):
+    ''' Read dataset from external file '''
+    data = {}
+    dataset = []
+    with open(sourceFile, 'r') as f:
+        lines = [line.rstrip('\n') for line in f]
+        for line in lines:
+            (outlook, temperature, humidity, wind, playtennis) = \
+                line.split(',')
+            data['outlook'] = outlook
+            data['temperature'] = temperature
+            data['humidity'] = humidity
+            data['wind'] = wind
+            data['playtennis'] = playtennis
+
+            dataset.append(data.copy())
+
+    return dataset
+
+
+def valuesOf(attribute, dataset):
+    ''' Get all possible values of an attribute in a dataset '''
+    values = set()
+    for d in dataset:
+        values.add(d[attribute])
+
+    return values
+
+
+def subset(attribute, value, dataset):
+    ''' Get subset of dataset in which attribute = value '''
+    s = []
+    for d in dataset:
+        if d[attribute] == value:
+            s.append(d.copy())
+
+    return s
+
+
+def count(targetAttribute, targetValue, dataset):
+    ''' Count how many target value appear in dataset '''
+    sum = 0
+    for d in dataset:
+        if d[targetAttribute] == targetValue:
+            sum += 1
+
+    return sum
+
+
+def entropy(targetAttribute, dataset):
+    ''' Compute entropy of a dataset with a target attribute '''
+    # Get all possible values of target attribute
+    targetValues = valuesOf(targetAttribute, dataset)
+
+    # Count how many each target value appear in dataset
+    proportions = [count(targetAttribute, targetValue, dataset)
+                   for targetValue in targetValues]
+
+    # Compute entropy
+    size = len(dataset)
+    e = 0.0
+    for p in proportions:
+        e -= p / size * math.log2(p / size)
+
+    return e
+
+
+def informationGain(attribute, targetAttribute, dataset):
+    ''' Compute gain information of an attribute in a dataset '''
+    # Compute entropy of original dataset
+    e = entropy(targetAttribute, dataset)
+    datasetSize = len(dataset)
+    ig = e
+
+    # Get all possible values of an attribute in dataset
+    values = valuesOf(attribute, dataset)
+    # Get all subset of dataset in which attribute = value
+    subsets = [subset(attribute, value, dataset) for value in values]
+
+    for s in subsets:
+        # Firstly, compute entropy of subset s
+        es = entropy(targetAttribute, s)
+        subsetSize = len(s)
+        ig -= subsetSize / datasetSize * es
+
+    return ig
